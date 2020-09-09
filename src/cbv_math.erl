5a6936a3032e6516101c23af0664a60c09592036
Pedro Silva <Pedro.Silva@Miniclip.com>
2020-09-09 14:18:07 +0100

    Add tests with coverage to cbv_math_server

--- src/cbv_math.erl
+++ src/cbv_math.erl
@@ -3,9 +3,13 @@
 -export([ eval/1
         ]).
 
-eval(N) when is_number(N) -> N;
-eval({add, N1, N2}) -> eval(N1) + eval(N2);
-eval({sub, N1, N2}) -> eval(N1) - eval(N2);
-eval({mul, N1, N2}) -> eval(N1) * eval(N2);
-eval({'div', N1, N2}) -> eval(N1) div eval(N2);
-eval({pow, _N1, _N2}) -> 1.
+eval(N) when is_number(N) -> ok(N);
+eval({add, N1, N2}) ->          ok(unwrap(eval(N1)) + unwrap(eval(N2)));
+eval({sub, N1, N2}) ->          ok(unwrap(eval(N1)) - unwrap(eval(N2)));
+eval({mul, N1, N2}) ->          ok(unwrap(eval(N1)) * unwrap(eval(N2)));
+eval({'div', N1, N2}) ->        ok(unwrap(eval(N1)) div unwrap(eval(N2)));
+eval({pow, _N1, _N2}) ->        ok(1);
+eval(_) -> {error, unsupported_expression }.
+
+ok(V) -> {ok, V}.
+unwrap({ok, V}) -> V.
--- test/cbv_math_server_SUITE.erl
+++ test/cbv_math_server_SUITE.erl
@@ -1,20 +1,39 @@
 -module(cbv_math_server_SUITE).
 
+-include_lib("stdlib/include/assert.hrl").
 -include_lib("common_test/include/ct.hrl").
 
--export([all/0, init_per_testcase/2, end_per_testcase/2]).
+-compile(export_all).
 
--export([
-  simple_eval_test/1
-]).
 
-all() -> [some_test].
+all() -> [
+  server_created_test,
+  eval_ok_test,
+  eval_err_test,
+  stats_test
+].
 
 init_per_testcase(_, Config) ->
-  Config.
+  ServerPid = cbv_math_server:start_link(),
+  [{server_pid, ServerPid}| Config].
 
-end_per_testcase(_, _Config) ->
+end_per_testcase(_, Config) ->
+  {ok, Pid} = ?config(server_pid, Config),
+  erlang:exit(Pid, normal),
   ok.
 
-simple_eval_test(Config) ->
-  ?assertEqual(42, cbv_math:eval(42)).
+server_created_test(Config) ->
+  {ok, Pid} = ?config(server_pid, Config),
+  ?assert(is_pid(Pid)).
+
+eval_ok_test(_Config) ->
+  ?assertMatch({ok, 42}, cbv_math_server:eval(42)).
+
+eval_err_test(_Config) ->
+  ?assertMatch({error, unsupported_expression}, cbv_math_server:eval(a)).
+
+stats_test(_Config) ->
+  ?assertMatch({ok, #{ fail := _Fail
+   , success := _Success
+   }}, cbv_math_server:stats()).
+
